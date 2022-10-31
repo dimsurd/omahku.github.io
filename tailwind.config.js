8e7e537dcfb9365500ad41748e282e36600ff2c1
Dimas-Kitsudev <dimassuryadilana@gmail.com>
2022-10-31 09:39:42 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    colors: {
+      ungu: "#4A6FFF",
+      unguTua: "#2451FF",
+      white: "#ffffff",
+      abu: "#95A2A9",
+      abuTua: "#9F9F9F",
+      abuMuda: "#D3D3D3",
+      abuBorder: "#E9EEF3",
+      black: "#2F2F2F",
+      black2: "#212121",
+      black3: "#373737",
+      orange: "#fa5a09",
+      beureum: "#FF0000",
+    },
+    extend: {
+      fontFamily: {
+        body: ["Montserrat"],
+      },
+    },
+  },
+  plugins: [],
+};
